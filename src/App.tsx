412f70c2ad20d0c57dfbaa0dbb8a427f300b1316
taewonnn <thisis_tw94@naver.com>
2026-01-08 15:42:57 +0900

    feat: 헤더 ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import SearchPage from './pages/SearchPage';
 import SearchResultPage from './pages/SearchResultPage';
 import WishListPage from './pages/WishListPage';
+import AppLayout from './layout/AppLayout';
 
 const router = createBrowserRouter([
-  { path: '/', element: <SearchPage /> },
-  { path: '/result', element: <SearchResultPage /> },
-  { path: '/wishlist', element: <WishListPage /> },
+  {
+    element: <AppLayout />,
+    children: [
+      { path: '/', element: <SearchPage /> },
+      { path: '/result', element: <SearchResultPage /> },
+      { path: '/wishlist', element: <WishListPage /> },
+    ],
+  },
 ]);
 
 export default function App() {
